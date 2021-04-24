44c67913884165aba942e75e39d482bfa2519ee1
Егор Шкарин <schkarin.egor@yandex.ru>
2021-04-24 21:24:11 +0300

    Добавьте файлы проекта.

---
+++ lab2.1/lab2.1.cpp
@@ -0,0 +1,100 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+TCHAR WinName[] = _T("lab2.1");
+
+int WINAPI _tWinMain(HINSTANCE This,		 // Дескриптор текущего приложения 
+	HINSTANCE Prev, 	// В современных системах всегда 0 
+	LPTSTR cmd, 		// Командная строка 
+	int mode) 		// Режим отображения окна
+{
+	HWND hWnd;		// Дескриптор главного окна программы 
+	MSG msg; 		// Структура для хранения сообщения 
+	WNDCLASS wc; 	// Класс окна
+	// Определение класса окна 
+	wc.hInstance = This;
+	wc.lpszClassName = WinName; 				// Имя класса окна 
+	wc.lpfnWndProc = WndProc; 					// Функция окна 
+	wc.style = CS_HREDRAW | CS_VREDRAW; 			// Стиль окна 
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); 		// Стандартная иконка 
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); 		// Стандартный курсор 
+	wc.lpszMenuName = NULL; 					// Нет меню 
+	wc.cbClsExtra = 0; 						// Нет дополнительных данных класса 
+	wc.cbWndExtra = 0; 						// Нет дополнительных данных окна 
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); 	// Заполнение окна белым цветом 
+
+
+	// Регистрация класса окна
+	if (!RegisterClass(&wc)) return 0;
+
+	// Создание окна 
+	hWnd = CreateWindow(WinName,			// Имя класса окна 
+		_T("lab2.1"), 		// Заголовок окна 
+		WS_OVERLAPPEDWINDOW, 		// Стиль окна 
+		CW_USEDEFAULT,				// x 
+		CW_USEDEFAULT, 				// y	 Размеры окна 
+		CW_USEDEFAULT, 				// width 
+		CW_USEDEFAULT, 				// Height 
+		HWND_DESKTOP, 				// Дескриптор родительского окна 
+		NULL, 						// Нет меню 
+		This, 						// Дескриптор приложения 
+		NULL); 					// Дополнительной информации нет 
+
+	ShowWindow(hWnd, mode); 				// Показать окно
+
+	// Цикл обработки сообщений 
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg); 		// Функция трансляции кодов нажатой клавиши 
+		DispatchMessage(&msg); 		// Посылает сообщение функции WndProc() 
+	}
+	return 0;
+}
+
+// Оконная функция вызывается операционной системой
+// и получает сообщения из очереди для данного приложения
+int count = 0;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HWND win2;
+	switch (message)		 // Обработчик сообщений
+	{
+	case WM_RBUTTONUP:
+		win2 = FindWindow(TEXT("lab2.2"), NULL);
+		SendMessage(win2, WM_USER + 3, (WPARAM)hWnd, 0);
+		InvalidateRect(hWnd, NULL, FALSE);
+		break;
+	case WM_LBUTTONUP:
+		win2 = FindWindow(TEXT("lab2.2"), NULL);
+		if (win2) 
+		{
+			if (count < 1)
+			{
+				MessageBox(hWnd, TEXT("Second window opened"), TEXT("Kek"), NULL);
+				SendMessage(win2, WM_USER + 1, (WPARAM)hWnd, 0);
+			}
+			else 
+			{
+				SendMessage(win2, WM_USER + 1, (WPARAM)hWnd, 0);
+			}
+		}
+		else 
+		{
+			MessageBox(hWnd, TEXT("Second window closed"), TEXT("Mirza"), NULL);
+			SetTimer(hWnd, 1, 10000, NULL);
+			PostMessage(hWnd, WM_DESTROY, 0, 0);
+		}
+		count++;
+		InvalidateRect(hWnd, NULL, FALSE);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		
+		break; // Завершение программы 
+	default: 			// Обработка сообщения по умолчанию 
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
